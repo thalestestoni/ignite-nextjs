a6bc750acc116e593f7fa6e2f2626738af8123cf
Thales Testoni <thalestestoni@gmail.com>
2021-08-17 21:38:13 -0300

    Removed global title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-
-          <title>spacetravelling.</title>
         </Head>
         <body>
           <Main />
