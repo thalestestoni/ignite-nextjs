1bee0330b5c10960dbf4b24dcf9f3d2347afe1ed
Thales Testoni <thalestestoni@gmail.com>
2021-08-28 12:34:25 -0300

    Estrutura + estilização pagina posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +29,85 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  // const posts = postsPagination.results;
+  
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+
+      <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <Image
+            src="/images/logo.svg"
+            alt="logo"
+            height={27}
+            width={239}
+          />
+        </div>
+      </header>
+
+      <main className={styles.contentContainer}>
+        <div className={styles.posts}>
+          {/* {posts.map(post => ( */}
+            <Link href={`/post/1`}>
+              <a key={`1`}>
+                <strong>Como utilizar Hooks</strong>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div>
+                  <span>
+                    <FiCalendar />
+                    <time>15 Mar 2021</time>
+                  </span>
+                  <span>
+                    <FiUser />
+                    <small>Thales Testoni</small>
+                  </span>
+                </div>
+              </a>
+            </Link>
+
+            <Link href={`/post/1`}>
+              <a key={`2`}>
+                <strong>Como utilizar Hooks</strong>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div>
+                  <span>
+                    <FiCalendar />
+                    <time>15 Mar 2021</time>
+                  </span>
+                  <span>
+                    <FiUser />
+                    <small>Thales Testoni</small>
+                  </span>
+                </div>
+              </a>
+            </Link>
+
+            <Link href={`/post/1`}>
+              <a key={`3`}>
+                <strong>Como utilizar Hooks</strong>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div>
+                  <span>
+                    <FiCalendar />
+                    <time>15 Mar 2021</time>
+                  </span>
+                  <span>
+                    <FiUser />
+                    <small>Thales Testoni</small>
+                  </span>
+                </div>
+              </a>
+            </Link>
+          {/* ))} */}
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
