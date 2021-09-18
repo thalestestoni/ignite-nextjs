301d04eb4daf167ed22ddffcd29a068d9d39abee
Thales Testoni <thalestestoni@gmail.com>
2021-09-18 11:15:29 -0300

    Component header initial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+       <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <Image
+            src="/images/logo.svg"
+            alt="logo"
+            height={27}
+            width={239}
+          />
+        </div>
+      </header>
+    </>
+  )
 }
